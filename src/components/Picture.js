ebdbfb3356725f8f989a5c6e7240b916091b6e28
poojachoudhary560 <poojachoudhary560@gmail.com>
2022-02-20 18:25:50 +0530

    2 files modified

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const Picture = (props) => {
   const [imageData, setImageData] = useState('');
+  // 8e31e45f4a0e8959d456ba2914723451b8262337f75bcea2e04ae535491df16d
   const apiUrl = `https://api.unsplash.com/photos/random/?client_id=NpzCe_cek_q1E4L6ntCx2kgwyuVBER6Qu1_dCi-OGDE`;
   useEffect(() => {
     fetch(apiUrl)
